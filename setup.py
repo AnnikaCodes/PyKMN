37ecddcded9504265af8e815aef11fc505783874
Annika L <annika0uwu@gmail.com>
2023-03-02 15:23:28 -0800

    Fix pacakging

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Sadly, it doesn't seem possible to express this in pyproject.toml.
 setup(
     cffi_modules=["build_bindings.py:ffibuilder"],
-    packages=["pykmn"],
+    packages=["pykmn.engine"],
     include_package_data=True
 )
