df90e5c651c6e3ae6c48e0875b9545efbd6a1ad3
github-action-benchmark <github@users.noreply.github.com>
2023-03-14 05:05:14 +0000

    add battles/second @pkmn benchmark (customBiggerIsBetter) benchmark result for 75c78ea2044f71275ecfe015d59e96df6f4e97da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,37 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678770313336,
+  "lastUpdate": 1678770314384,
   "repoUrl": "https://github.com/AnnikaCodes/PyKMN",
   "entries": {
     "battles/second @pkmn benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "annika0uwu@gmail.com",
+            "name": "Annika L",
+            "username": "AnnikaCodes"
+          },
+          "committer": {
+            "email": "annika0uwu@gmail.com",
+            "name": "Annika L",
+            "username": "AnnikaCodes"
+          },
+          "distinct": true,
+          "id": "75c78ea2044f71275ecfe015d59e96df6f4e97da",
+          "message": "GitHub Actions: Attempt to automatically benchmark",
+          "timestamp": "2023-03-13T22:03:31-07:00",
+          "tree_id": "f4de53d00bbb46b3c13db89864b4b292c133a034",
+          "url": "https://github.com/AnnikaCodes/PyKMN/commit/75c78ea2044f71275ecfe015d59e96df6f4e97da"
+        },
+        "date": 1678770312947,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "PyKMN FFI Benchmark",
+            "value": 14370,
+            "unit": "battles/second"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
