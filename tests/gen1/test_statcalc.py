1e52ce073c340b603a0aceb5300c2ab7afe4b13b
Annika L <annika0uwu@gmail.com>
2023-03-26 18:54:30 -0700

    Add tests for stat calculation

--- tests/gen1/test_statcalc.py
+++ tests/gen1/test_statcalc.py
@@ -20,3 +20,28 @@ class TestGen1StatCalculation(unittest.TestCase):
             statcalc(gengar_stats['hp'], dv=15, experience=65535, level=100, is_HP=True),
             323,
         )
+
+    def test_vulpix_hp(self):
+        """Vulpix's HP stat is calculated correctly."""
+        vulpix_stats = SPECIES['Vulpix']['stats']
+        self.assertEqual(
+            statcalc(vulpix_stats['hp'], level=97, dv=5, is_HP=True),
+            251,
+        )
+
+    @unittest.skip("outstanding bug that needs investigation")
+    def test_porygon_hp(self):
+        """Porygon's HP stat is calculated = to JS."""
+        porygon_stats = SPECIES['Porygon']['stats']
+        self.assertEqual(
+            statcalc(porygon_stats['hp'], level=100, is_HP=True, experience=25590),
+            310,
+        )
+
+    def test_psyduck_hp(self):
+        """Psyduck's HP stat is calculated = to JS."""
+        psyduck_stats = SPECIES['Psyduck']['stats']
+        self.assertEqual(
+            statcalc(psyduck_stats['hp'], level=10, is_HP=True),
+            39,
+        )
